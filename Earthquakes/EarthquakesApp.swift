f986d9912e5b9d65d419da242643915fbea1cb0b
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-12-17 19:42:40 +0100

    Initial commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Pierre-Louis ML on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
